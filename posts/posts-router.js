6a39400683cc62ff1f3b7114db1a41448a77cd85
James Basile <jbasile145@gmail.com>
2019-03-19 18:41:10 -0400

    delete functionality added, post with id5 was removed

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -36,15 +36,32 @@ router.get('/:id', (req, res) => {
     const id = req.params.id;
 
     Posts.findById(id)
-        .then( posts => {
-            if (!posts.length) {
+        .then( post => {
+            if (!post.length) {
                 res.status(404).json({ message: "The post with the specified ID does not exist." })
             } else {
-                res.status(200).json(posts)
+                res.status(200).json(post)
             }
         })
         .catch(err => {
             res.status(500).json({ error: "The post information could not be retrieved." })
+        })
+})
+
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    Posts.remove(id)
+        .then(post => {
+            if(!post) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                res.status(200).json(post)
+            }
+        })
+        .catch( err => {
+            res.status(500).json({ error: "The post could not be removed" })
         })
 })
 
