513ebacc1e9303ddd1ba22cb68ce07700461d696
James Basile <jbasile145@gmail.com>
2019-03-19 17:49:13 -0400

    POST endpoint complete

---
+++ posts/posts-router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const Posts = require('../data/db');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+    if (!req.body.title || !req.body.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        Posts.insert(req.body)
+            .then( post => {
+                Posts.findById(post.id)
+                .then(newPost => {
+                    res.status(200).json(newPost);
+                })
+            })
+            .catch(err => {
+                res.status(500).json({ error: "There was an error while saving the post to the database" })
+            })
+    }
+})
